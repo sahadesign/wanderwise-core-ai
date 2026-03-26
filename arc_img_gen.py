466944f372b7f0b60f4943139c6af26582296f89
Sahana V Kumar <77962951+sahadesign@users.noreply.github.com>
2026-03-26 21:06:15 +0530

    Add arch image generation code

---
+++ arc_img_gen.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from main import WanderWiseAgent
+load_dotenv()
+
+def generate_diagram(agent_app, filename="architecture_flow.png"):
+    try:
+        png_bytes = agent_app.get_graph().draw_mermaid_png()
+        
+        with open(filename, "wb") as f:
+            f.write(png_bytes)
+        print(f"Architecture diagram saved as {filename}")
+        
+    except Exception as e:
+        print(f"Could not generate PNG: {e}")
+        print(agent_app.get_graph().draw_mermaid())
+
+
+agent = WanderWiseAgent(
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    geo_api_key=os.getenv("GEOAPIFY_API_KEY"),
+    weather_api_key=os.getenv("OPENWEATHER_API_KEY"),
+)
+
+generate_diagram(agent.app)
